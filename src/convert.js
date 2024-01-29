8e40d34ac48ac257daedda954c6fc67d010ed3f4
Kael Zhang <i+github@kael.me>
2024-01-29 23:08:15 +0800

    WIP

---
+++ src/convert.js
@@ -0,0 +1,63 @@
+
+const {
+  SIN,
+  COS
+} = require('./rotate')
+
+// Get the z-y-x rotation matrix from the given x, y and z rotations
+const ZYX_TO_MATRIX = (rz, ry, rx) => [
+  [
+    COS(ry) * COS(rz),
+    COS(ry) * SIN(rz),
+    -SIN(ry)
+  ],
+  [
+    SIN(rx) * SIN(ry) * COS(rz) - COS(rx) * SIN(rz),
+    SIN(rx) * SIN(ry) * SIN(rz) + COS(rx) * COS(rz),
+    SIN(rx) * COS(ry)
+  ],
+  [
+    COS(rx) * SIN(ry) * COS(rz) + SIN(rx) * SIN(rz),
+    COS(rx) * SIN(ry) * SIN(rz) - SIN(rx) * COS(rz),
+    COS(rx) * COS(ry)
+  ]
+]
+
+// Get the x-y-z rotation matrix from the given x, y and z rotations
+const XYZ_TO_MATRIX = (rx, ry, rz) => [
+  [
+    COS(ry) * COS(rz),
+    SIN(rx) * SIN(ry) * COS(rz) + COS(rx) * SIN(rz),
+    - COS(rx) * SIN(ry) * COS(rz) + SIN(rx) * SIN(rz)
+  ],
+  [
+    - COS(ry) * SIN(rz),
+    - SIN(rx) * SIN(ry) * SIN(rz) + COS(rx) * COS(rz),
+    COS(rx) * SIN(ry) * SIN(rz) + SIN(rx) * COS(rz)
+  ],
+  [
+    SIN(ry),
+    - SIN(rx) * COS(ry),
+    COS(rx) * COS(ry)
+  ]
+]
+
+// Get the z-y-x rotations in degrees from the given rotation matrix
+const MATRIX_TO_XYZ = (matrix) => {s s s s s s s s s s s
+  let ry = Math.asin(matrix[2][0])
+  const cos_ry = Math.cos(ry)
+
+  let rx = Math.atan(- matrix[2][1] / matrix[2][2])
+  let rz = Math.atan(- matrix[1][0] / matrix[0][0])
+
+  rx = rx * (180 / Math.PI)
+  ry = ry * (180 / Math.PI)
+  rz = rz * (180 / Math.PI)
+
+  return [rx, ry, rz]
+}
+
+const ZYX_TO_XYZ = exports.ZYX_TO_XYZ = (rz, ry, rx) => {
+  const matrix = ZYX_TO_MATRIX(rz, ry, rx)
+  return MATRIX_TO_XYZ(matrix)
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
+const {
+  ROTATE,
+  ADD,
+  MUL,
+  REVERSE
+} = require('./rotate')
+
+const {
+  ZYX_TO_XYZ
+} = require('./convert')
+
 const COOR_ALPHA = [1, 1, -1]
+const ROTATE_ALPHA = [1, -1, 1]
+
+const SWITCH = c => MUL(c, COOR_ALPHA)
+
+const oo = [0, 0, 0]
+
+const nodes = [
+  [
+    // origin
+    [0, 3100, 0],
+    // rotation
+    [-45, 0, 30]
+  ],
+  [
+    [-559.96, 4082.76, 1129.68],
+    [-72.81, 28.88, 38.5]
+  ]
+]
+
+nodes.forEach(node => {
+  const o = SWITCH(node[0])
+  const r = MUL(node[1], -1)
+
+  const xyz = ZYX_TO_XYZ(...REVERSE(r, true))
+
+  const vo = ROTATE(
+    ADD(oo, MUL(o, - 1)),
+    r,
+    true,
+    true
+  )
+
+  // console.log(ADD(oo, MUL(o, - 1)), xyz)
+
+  // const vo = ROTATE(
+  //   ADD(oo, MUL(o, - 1)),
+  //   xyz
+  // )
+
+  console.log(
+    SWITCH(vo),
+    MUL(SWITCH(vo), 2),
+    MUL(xyz, ROTATE_ALPHA)
+  )
+})
