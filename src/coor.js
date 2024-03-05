843a261dde4e4522687d23856c8e835cb055f73a
Kael Zhang <i+github@kael.me>
2024-03-05 23:37:09 +0800

    adds coor.js

---
+++ src/coor.js
@@ -0,0 +1,29 @@
+const {
+  UNIT_VECTORS,
+  ROTATE,
+  DOT
+} = require('./rotate')
+
+
+const get_unit_vectors = (...r) => [
+  ROTATE(UNIT_VECTORS[0], r),
+  ROTATE(UNIT_VECTORS[1], r),
+  ROTATE(UNIT_VECTORS[2], r)
+]
+
+// Change a vector from the global coordinate system
+//   to the local coordinate system
+const GLOBAL_VECTOR_TO_LOCAL = (v, rotate) => {
+  const unit_vectors = get_unit_vectors(...rotate)
+
+  return [
+    DOT(v, unit_vectors[0]),
+    DOT(v, unit_vectors[1]),
+    DOT(v, unit_vectors[2])
+  ]
+}
+
+
+module.exports = {
+  GLOBAL_VECTOR_TO_LOCAL
+}
